0578b45ada39db2474111048d80d12d460163992
gurdeep4git <gurdeep.singh@prmtechnologies.com>
2019-10-08 23:59:23 +0530

    filtered data showing

--- src/scripts/Utilities/UI/AutocompleteComponent/autocompleteComponent.ts
+++ src/scripts/Utilities/UI/AutocompleteComponent/autocompleteComponent.ts
@@ -5,21 +5,26 @@ export class AutocompleteComponent {
     private $currentAutocompleteComponentInput: JQuery<HTMLElement>;
     private $currentAutocompleteComponentList: JQuery<HTMLElement>;
 
-    private data: Object[];
-
     private config: AutocompleteComponentConfig = {
         id: null,
         data: null
     };
 
     init(autocompleteComponentConfig: AutocompleteComponentConfig) {
+        this.config.id = autocompleteComponentConfig.id;
+        this.config.data = autocompleteComponentConfig.data;
+
         //set current input as JqueryElement
         this.$currentAutocompleteComponentInput = $(
             `#${autocompleteComponentConfig.id}`
         );
 
-        this.config.id = autocompleteComponentConfig.id;
-        this.config.data = autocompleteComponentConfig.data;
+        const listContainer = `<div class="autocomplete-list-container" id=${this.config.id}AutocompleteList></div>`;
+        this.$currentAutocompleteComponentInput.after(listContainer);
+
+        this.$currentAutocompleteComponentList = this.$currentAutocompleteComponentInput.next(
+            ".autocomplete-list-container"
+        );
 
         this.bindEvents();
     }
@@ -38,19 +43,8 @@ export class AutocompleteComponent {
             return e.title.includes(value);
         });
 
-        console.log(filterData);
-
-        const obj = {
-            id: this.config.id,
-            data: filterData
-        };
-
-        this.$currentAutocompleteComponentInput.after(
-            autocompleteComponentTemplate(obj)
-        );
-
-        this.$currentAutocompleteComponentList = this.$currentAutocompleteComponentInput.next(
-            ".autocomplete-list-container"
+        this.$currentAutocompleteComponentList.html(
+            autocompleteComponentTemplate(filterData)
         );
 
         this.$currentAutocompleteComponentList.show();
