ddb437d3012cebbbd801573778b25848d05b559b
gurdeep4git <gurdeep.singh@prmtechnologies.com>
2019-10-08 23:45:25 +0530

    workflow changes for autocomplete

--- src/scripts/Utilities/UI/AutocompleteComponent/autocompleteComponent.ts
+++ src/scripts/Utilities/UI/AutocompleteComponent/autocompleteComponent.ts
@@ -2,51 +2,57 @@ import "../../../../sass/autocompleteComponent.scss";
 import * as autocompleteComponentTemplate from "../../../../scripts/Handlebars/templates/UI/AutocompleteComponent/autocompleteComponent.hbs";
 
 export class AutocompleteComponent {
-    private $currentAutocompleteComponentWrapper: JQuery<HTMLElement>;
-    private $currentAutocompleteComponent: JQuery<HTMLElement>;
     private $currentAutocompleteComponentInput: JQuery<HTMLElement>;
     private $currentAutocompleteComponentList: JQuery<HTMLElement>;
 
+    private data: Object[];
+
+    private config: AutocompleteComponentConfig = {
+        id: null,
+        data: null
+    };
+
     init(autocompleteComponentConfig: AutocompleteComponentConfig) {
-        //set wrapper container as JqueryElement
-        this.$currentAutocompleteComponentWrapper = $(
-            `#${autocompleteComponentConfig.container}`
+        //set current input as JqueryElement
+        this.$currentAutocompleteComponentInput = $(
+            `#${autocompleteComponentConfig.id}`
         );
 
-        //Bind autocomplete template to wrapper
-        this.$currentAutocompleteComponentWrapper.html(
-            autocompleteComponentTemplate(autocompleteComponentConfig)
-        );
-
-        //Set elements of autocomplete
-        this.extractElements(autocompleteComponentConfig);
+        this.config.id = autocompleteComponentConfig.id;
+        this.config.data = autocompleteComponentConfig.data;
 
         this.bindEvents();
     }
 
-    private extractElements(
-        autocompleteComponentConfig: AutocompleteComponentConfig
-    ) {
-        this.$currentAutocompleteComponent = $(
-            `#${autocompleteComponentConfig.id}Autocomplete`
-        );
-        this.$currentAutocompleteComponentInput = this.$currentAutocompleteComponent.find(
-            ".autocomplete-input"
-        );
-        this.$currentAutocompleteComponentList = this.$currentAutocompleteComponent.find(
-            ".autocomplete-list-container"
-        );
-    }
-
     private bindEvents() {
-        this.$currentAutocompleteComponentWrapper.on(
-            "keyup",
-            this.$currentAutocompleteComponentInput,
-            () => this.onSearch()
+        this.$currentAutocompleteComponentInput.on("keyup", () =>
+            this.onSearch()
         );
     }
 
     private onSearch(): void {
+        let value = this.$currentAutocompleteComponentInput.val();
+
+        const filterData = this.config.data.filter(e => {
+            //@ts-ignore
+            return e.title.includes(value);
+        });
+
+        console.log(filterData);
+
+        const obj = {
+            id: this.config.id,
+            data: filterData
+        };
+
+        this.$currentAutocompleteComponentInput.after(
+            autocompleteComponentTemplate(obj)
+        );
+
+        this.$currentAutocompleteComponentList = this.$currentAutocompleteComponentInput.next(
+            ".autocomplete-list-container"
+        );
+
         this.$currentAutocompleteComponentList.show();
     }
 }
@@ -54,5 +60,4 @@ export class AutocompleteComponent {
 export class AutocompleteComponentConfig {
     id: string;
     data: Object[];
-    container: string;
 }
--- src/scripts/autocomplete/autocomplete.ts
+++ src/scripts/autocomplete/autocomplete.ts
@@ -5,16 +5,25 @@ import {
 } from "../Utilities/UI/AutocompleteComponent/autocompleteComponent";
 
 class Autocomplete {
-    data = [{ id: 1, title: "title 1" }, { id: 2, title: "title 2" }];
+    data = [
+        { id: 1, title: "france" },
+        { id: 2, title: "london" },
+        { id: 3, title: "paris" },
+        { id: 4, title: "usa" },
+        { id: 5, title: "canada" },
+        { id: 6, title: "india" }
+    ];
 
     constructor() {
-        const autocompleteComponentConfig = new AutocompleteComponentConfig();
-        autocompleteComponentConfig.id = "my1";
-        autocompleteComponentConfig.data = this.data;
-        autocompleteComponentConfig.container = "autocompleteContainer";
+        $(".autocomplete-input").each((_i, input) => {
+            const id = $(input).attr("id");
+            const autocompleteComponentConfig = new AutocompleteComponentConfig();
+            autocompleteComponentConfig.id = id;
+            autocompleteComponentConfig.data = this.data;
 
-        const autocompleteComponent = new AutocompleteComponent();
-        autocompleteComponent.init(autocompleteComponentConfig);
+            const autocompleteComponent = new AutocompleteComponent();
+            autocompleteComponent.init(autocompleteComponentConfig);
+        });
     }
 }
 new Autocomplete();
